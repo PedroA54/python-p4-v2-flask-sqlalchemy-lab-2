31404ff7eba6a5de80d0213dfc1206fb7cee193a
Pedro Alarcon <pedroa54@icloud.com>
2024-05-13 18:22:35 -0500

    adds complete lab

--- server/models.py
+++ server/models.py
@@ -4,29 +4,58 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
-    __tablename__ = 'customers'
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
+
+    # Relationships
+    customer = db.relationship("Customer", back_populates="reviews")
+    item = db.relationship("Item", back_populates="reviews")
+
+    serialize_rules = ("-customer.reviews", "-item.reviews")
+
+
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # Relationships
+
+    reviews = db.relationship("Review", back_populates="customer")
+
+    # association proxy
+
+    items = association_proxy("reviews", "item", creator=lambda item: Review(item=item))
+
     def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
+        return f"<Customer {self.id}, {self.name}>"
 
 
-class Item(db.Model):
-    __tablename__ = 'items'
+class Item(db.Model, SerializerMixin):
+    __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # Relationships
+
+    reviews = db.relationship("Review", back_populates="item")
+
     def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+        return f"<Item {self.id}, {self.name}, {self.price}>"
